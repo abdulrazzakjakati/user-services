ad78a59488454cc32d68cbaa3ad7afa9ba81aa47
abdulrazzak jakati <abdulrazzak.jakati@gmail.com>
2026-04-19 00:42:49 +0530

    v1 sonar fixed

--- src/main/java/com/codedecode/userinfo/config/WebConfig.java
+++ src/main/java/com/codedecode/userinfo/config/WebConfig.java
@@ -6,20 +6,22 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@SuppressWarnings("squid:S1186") // Suppress specific SonarQube rules
 public class WebConfig {
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:3000", "http://34.204.163.241", "https://happymoments.itrtechsystems.com")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:4200")
+                        .allowedOriginPatterns("*") // 👈 public
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
--- src/main/java/com/codedecode/userinfo/service/UserService.java
+++ src/main/java/com/codedecode/userinfo/service/UserService.java
@@ -2,7 +2,6 @@ package com.codedecode.userinfo.service;
 
 import com.codedecode.userinfo.dto.UserDTO;
 import com.codedecode.userinfo.entity.User;
-import com.codedecode.userinfo.mapper.UserMapper;
 import com.codedecode.userinfo.mapper.UserMapperInterface;
 import com.codedecode.userinfo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -27,14 +26,6 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
         return userMapper.userToUserDTO(user);
     }
-
-//    public boolean deleteUserById(long id) {
-//        if (userRepository.existsById(id)) {
-//            userRepository.deleteById(id);
-//            return true;
-//        }
-//        return false;
-//    }
 
     public List<UserDTO> getAllUsers() {
         List<User> userList = userRepository.findAll();
